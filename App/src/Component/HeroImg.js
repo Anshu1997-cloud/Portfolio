3a11af100411a60323feafb276c5112b21311ac5
Anshuman Chaturvedi <anshumanchaturvedi91@gmail.com>
2024-06-18 17:34:47 +0530

    added new chnages

--- App/src/Component/HeroImg.js
+++ App/src/Component/HeroImg.js
@@ -1,35 +1,48 @@
 import "./HeroImgStyles.css";
 import PortFolioPic3 from "../assets/PortFolioPic3.avif";
 import Me from "../assets/Me.jpeg";
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from "react";
+import Resume from '../assets/Anshuman_Resume .pdf'
+import { Link} from "react-router-dom";
 
 const HeroImg = () => {
+
+  const handleResumeClick = () => {
+    window.open("https://drive.google.com/file/d/1Q8ZCuP8g4EetL_MqXSdKpjFDO_XN5AYJ/view?usp=sharing", "_blank");
+  };
+  
   return (
     <div className="hero">
-       <div className="mask">
-            <img className="into-img"
-            src={PortFolioPic3} alt="PortFolioPic3"
-            />
-            </div>
-            <div className="content">
-                <img className="img" src={Me} alt="Me.jpg"/>
-                  <p>ANSHUMAN CHATURVEDI</p>
-                  <h1>FRONT-END DEVELOPER.</h1>
-                  <div>
-                    <a href="#project" className="btn">Project</a>
-                    <a href="#contact" className="btn btn-light">Contact</a>
-                   <button className="lean-more">
-                   <a href="https://drive.google.com/file/d/1spZFAoiTXMPaTUCk2qSnvi46c5IjvN0o/view" target="_blank" download="Anshuman_Chaturvedi_Resume.pdf">
-                    <span className="circle" aria-hidden="true"></span>
-                      <span className="icon arrow"></span>
-                      <span className="button-text">Resume</span>
-                      </a>
-                   </button>
-                  </div>
-            </div>
+      <div className="mask">
+        <img className="into-img" src={PortFolioPic3} alt="PortFolioPic3" />
+      </div>
+      <div className="content">
+        <img className="img" src={Me} alt="Me.jpg" />
+        <p>ANSHUMAN CHATURVEDI</p>
+        <h1>FRONT-END DEVELOPER.</h1>
+        <div>
+          <a href="#project" className="btn">
+            Projects
+          </a>
+          <a href="#contact" className="btn btn-light">
+            Contact
+          </a>
+          <button
+            onClick={handleResumeClick } className="btn"
+          >
+            <a
+              href={Resume}
+              target="_blank"
+              download="Anshuman_Resume.pdf"
+              className="lean-more"
+            >
+              <span className="button-text">Resume</span>
+            </a>
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default HeroImg;
